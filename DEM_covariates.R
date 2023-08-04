78129d3a18846120623985c8630cabe38e6e3215
Milcah-Kirinyet <109551320+Milcah-Kirinyet@users.noreply.github.com>
2023-08-04 22:34:17 +0100

    DEM

--- DEM_covariates.R
+++ DEM_covariates.R
@@ -4,17 +4,6 @@ library(tidyverse)
 library(raster)
 library(terra)
 
-# Read the shapefile of the study area and other related data
-inpath<-"C:\\workspace\\Kirinyet-development\\data\\CSA_Baseline_SOC\\"
-#CAA<-st_read(paste0(inpath,"Milcah\\CSA_Conservation_Agreement_Areas.shp"))
-CSA_NL <-st_read(paste0(inpath, "Milcah\\CSA_NAtional_Landscapes.shp"))
-#SOC_locations<-st_read(paste0(inpath, "Milcah\\SOC_GPS_Locations.shp"))
-
-mzimvubu <- CSA_NL[CSA_NL$Name == "Mzimvubu Landscape", ] #subset NLA to mzimvubu
-plot(mzimvubu["Name"], main= "Mzimvubu")
-
-mzimvubu_sf <- as(mzimvubu, "sf")
-
 #### Elevation,Apect,TWI,Slope ####
 #https://cran.r-project.org/web/packages/elevatr/vignettes/introduction_to_elevatr.html
 south_africa_sp <- rgdal::readOGR("C:\\workspace\\Kirinyet-development\\ndvi_images\\South Africa.shp") # larger extent is better
